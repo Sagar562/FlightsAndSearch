1a1c4a181fff4b30d6d9e5d7e17557f9bd5e48ac
sagar <vyassagar4321@gmail.com>
2025-06-08 20:17:16 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const { City } = require('../models/index');
 
 class CityRepository {
@@ -10,17 +11,40 @@ class CityRepository {
         }
     }
 
-    async deleteCity({ cityId }) {
+    async deleteCity(cityId) {
         try {
             await City.destroy({
                 where: {
                     id: cityId
                 }
             });
+            return true;
         } catch(error) {
             throw {error};
         }
     } 
+
+    async updateCity(cityId, { data }) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                id: cityId
+                }
+            });
+            return city;
+        } catch(error) {
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch(error) {
+            throw {error};
+        }
+    }
 }
 
 module.exports = CityRepository;
